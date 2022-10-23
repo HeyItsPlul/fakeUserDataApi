694a9bdc6c51ae80a8098425e2ca2fba41dd9396
HeyItsPlul <PlulCoding@mail.com>
2022-10-23 07:52:23 +0200

    Added Commenting

--- src/Routes/video.js
+++ src/Routes/video.js
@@ -7,7 +7,7 @@ router.get('/findMany/:id', async function(req, res) {
     res.send(response)
 })
 
-router.put('/upload', async function(req, res) {
+router.post('/upload', async function(req, res) {
     let post = new videoSchema({
         "link": req.body.link,
         "title": req.body.title,
@@ -17,6 +17,19 @@ router.put('/upload', async function(req, res) {
 
     let uploadSave = await post.save()
     res.json(uploadSave)
+})
+
+router.get('/:id', async function(req, res) {
+    let response = await videoSchema.findById(req.params.id)
+    res.send(response)
+})
+
+router.patch('/comment/:id', async function(req, res) {
+    let response = await videoSchema.update({
+        _id : req.params.id},
+        { $push: { "comments": req.body.comment}}) 
+
+    res.json(response)
 })
 
 module.exports = router
